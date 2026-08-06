5f7a6656d2e22ad61e768d8075ad43957fdb667b
Jayden <jayden.kim1824@gmail.com>
2026-08-06 17:42:02 -0500

    Load inbox with docked compose popup instead of full-page compose

--- docs/redirect.js
+++ docs/redirect.js
@@ -4,12 +4,12 @@ const mailto = params.get("uri");
 if (mailto) {
   const url = new URL(mailto);
   const to = url.pathname;
-  const gmail = new URLSearchParams({ view: "cm", fs: "1", to });
+  const gmail = new URLSearchParams({ to });
   if (url.searchParams.get("subject")) gmail.set("su", url.searchParams.get("subject"));
   if (url.searchParams.get("body")) gmail.set("body", url.searchParams.get("body"));
   if (url.searchParams.get("cc")) gmail.set("cc", url.searchParams.get("cc"));
   if (url.searchParams.get("bcc")) gmail.set("bcc", url.searchParams.get("bcc"));
-  location.replace("https://mail.google.com/mail/?" + gmail.toString());
+  location.replace("https://mail.google.com/mail/u/0/?" + gmail.toString() + "#inbox?compose=new");
 } else {
   navigator.registerProtocolHandler(
     "mailto",
