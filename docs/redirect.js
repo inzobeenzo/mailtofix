a5e7fbb7ad2e83d011f7130b794a26b5c0f6e67f
Jayden <jayden.kim1824@gmail.com>
2026-08-06 17:54:16 -0500

    Revert to full-page compose for reliable field prefill

--- docs/redirect.js
+++ docs/redirect.js
@@ -4,12 +4,12 @@ const mailto = params.get("uri");
 if (mailto) {
   const url = new URL(mailto);
   const to = url.pathname;
-  const gmail = new URLSearchParams({ to });
+  const gmail = new URLSearchParams({ view: "cm", to });
   if (url.searchParams.get("subject")) gmail.set("su", url.searchParams.get("subject"));
   if (url.searchParams.get("body")) gmail.set("body", url.searchParams.get("body"));
   if (url.searchParams.get("cc")) gmail.set("cc", url.searchParams.get("cc"));
   if (url.searchParams.get("bcc")) gmail.set("bcc", url.searchParams.get("bcc"));
-  location.replace("https://mail.google.com/mail/u/0/?" + gmail.toString() + "#inbox?compose=new");
+  location.replace("https://mail.google.com/mail/u/0/?" + gmail.toString());
 } else {
   navigator.registerProtocolHandler(
     "mailto",
